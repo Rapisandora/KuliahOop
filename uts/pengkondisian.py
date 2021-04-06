96ab10206e6dae3b3a8627b3fd778d8ec7d17984
Rapisandora <rapisandora13@gmail.com>
2021-04-06 20:15:14 +0700

    initial code

---
+++ uts/pengkondisian.py
@@ -0,0 +1,12 @@
+a = "hutan"
+if a == "hutan":
+    print ("kota harus dibangun")
+else:
+    print ("hutan dan kota harus dibangun")
+b = "kota"
+if b == "hutan":
+    print ("kota harus dibangun")
+elif b=="hutan":
+    print ("hutan harus dijaga")
+else :
+    print ("hutan dan kota harus dibangun")
---
+++ uts/set.py
@@ -0,0 +1,6 @@
+data=(1,1,1,1,1,2,2,2,3)
+print (data)
+listdata=[1,1,1,1,1,1,2,2,2,2,3]
+print (listdata)
+set(listdata)
+print set (listdata)
